47c534633608395b0d7dc0add26f6868921449a3
zaytsev <zaicev@mniti.ru>
2024-06-26 10:19:12 +0300

    update

--- email-editor/src/App.jsx
+++ email-editor/src/App.jsx
@@ -9,6 +9,7 @@ export function App() {
   return (
     <>
       <div>
+          {document.getElementById()}
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
