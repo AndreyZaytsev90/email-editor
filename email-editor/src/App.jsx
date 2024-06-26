876b0a21cd78922cfaba73cc8253e8c960581abc
zaytsev <zaicev@mniti.ru>
2024-06-26 16:52:53 +0300

    update

--- email-editor/src/App.jsx
+++ email-editor/src/App.jsx
@@ -2,10 +2,7 @@ import styles from './App.module.css'
 import {Details} from "./assets/Details.jsx";
 import {useCallback, useEffect, useRef, useState} from "react";
 
-const MENU = [
-    {name: 'add', link: './'},
-    {name: 'delete', link: './'},
-]
+const MENU = [{name: 'add', link: './'}, {name: 'delete', link: './'},]
 
 const isAuth = true
 
@@ -13,13 +10,15 @@ export function App() {
     /*const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
     const [buttonText, setButtonText] = useState('')*/
+    // Хук useState для локального отслеживания и изменения состояния
     const [details, setDetails] = useState({
-        isLoading: true,
-        title: 'useEffect',
-        description: 'Hello, React!',
-        buttonText: 'Click'
+        isLoading: true, title: 'useEffect', description: 'Hello, React!', buttonText: 'Click'
     })
 
+    const [count, setCount] = useState(0)
+    const [multi, setMulti] = useState(10)
+
+    // Хук useEffect для мгновенной загрузки чего-либо во время рендеринга приложения (можно прицепить зависимость для повторной загрузки)
     useEffect(() => {
         const timeout = setTimeout(() => {
             setDetails(prev => ({...prev, isLoading: false}))
@@ -30,7 +29,7 @@ export function App() {
         }
     }, [])
 
-
+    // Хук useRef для привязки в любому элементу дом-дерева и последующего взаимодействия с ним
     const imageRef = useRef(null)
 
     const onClick = () => {
@@ -42,27 +41,35 @@ export function App() {
 
     }
 
+    // Хук useCallback для кэширования и оптимизации отрисовки
+
     const handlerLoading = useCallback(() => {
         setDetails(prev => ({...prev, isLoading: !prev.isLoading}))
     }, [])
 
-    return (
-        <>
-            <div className={styles.layout}>
-                <img ref={imageRef} src="/react.svg" width={300} alt=""/>
-                <br/>
-                <button onClick={onClick}>Change image</button>
-                {details.isLoading && <p>Loading...</p>}
+    const result = count + multi
 
-                <Details MENU={MENU}
-                         isAuth={isAuth}
-                         details={details}
-                         setDetails={setDetails}
-                         handlerLoading={handlerLoading}
-                />
+    return (<>
+        <div className={styles.layout}>
+            <img ref={imageRef} src="/react.svg" width={300} alt=""/>
+            <br/>
+            <div>Результат: {result}</div>
+            <button onClick={()=> setCount(count+1)}>Увеличить счетчик</button>
+            <button onClick={()=> setMulti(multi+10)}>Увеличить множитель</button>
 
-            </div>
-        </>
-    )
+            <br/>
+            <br/>
+            <button onClick={onClick}>Change image</button>
+            {details.isLoading && <p>Loading...</p>}
+
+            <Details MENU={MENU}
+                     isAuth={isAuth}
+                     details={details}
+                     setDetails={setDetails}
+                     handlerLoading={handlerLoading}
+            />
+
+        </div>
+    </>)
 }
 
